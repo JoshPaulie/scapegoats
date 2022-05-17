c207ac69ba647032d0e37abb7fa8c09dc295bb7d
JoshPaulie <joshpaullee@gmail.com>
2022-05-17 11:06:17 -0500

    More details

--- bingo/script.js
+++ bingo/script.js
@@ -1,13 +1,13 @@
 "use strict";
 
 const task_descriptions = {
-    "10WildyBossKills": "Kill 10 <a href='https://oldschool.runescape.wiki/w/Wilderness#Bosses target='_blank''>Wilderness Bosses</a>",
-    "500kLootKey": "Earn a PK Loot Key worth 500k GP or more",
-    "10MedClue": "Complete 10 Medium+ Clue Scrolls",
-    "1mItemFlip": "Flip 1 item on the Grand Exchange for a profit of 1m+",
-    "SkillingPet": "Earn any <a href='https://oldschool.runescape.wiki/w/Pet#Skilling_pets target='_blank''>skilling pet</a>",
+    "10WildyBossKills": "Kill 10 <a href='https://oldschool.runescape.wiki/w/Wilderness#Bosses' target='_blank'>Wilderness Bosses</a>",
+    "500kLootKey": "Earn a PK <a href='https://oldschool.runescape.wiki/w/Loot_key' target='_blank'>Loot Key</a> worth 500k GP or more",
+    "10MedClue": "Complete 10 Medium+ <a href='https://oldschool.runescape.wiki/w/Clue_scroll' target='_blank'>Clue Scrolls</a>",
+    "1mItemFlip": "<a href='https://oldschool.runescape.wiki/w/Merchanting' target='_blank'>Flip</a> 1 item on the Grand Exchange for a profit of 1m+",
+    "SkillingPet": "Earn any <a href='https://oldschool.runescape.wiki/w/Pet#Skilling_pets' target='_blank''>skilling pet</a>",
 
-    "2CarvedBones": "Get a <a href='https://oldschool.runescape.wiki/w/Curved_bone' target='_blank'>curved bone</a> drop",
+    "2CurvedBones": "Get 2 <a href='https://oldschool.runescape.wiki/w/Curved_bone' target='_blank'>curved bone</a> drop",
     "10QP": "Earn 10 additional Quest Points (as a team)",
     "50SlayerBossKC": "Get 50 <a href='https://oldschool.runescape.wiki/w/Boss#Slayer_bosses' target='_blank'>Slayer Boss</a> kills",
     "GnomeScarfDrop": "Get a <a href='https://oldschool.runescape.wiki/w/Gnome_scarf' target='_blank'>Gnome Scarf</a> drop",
@@ -19,18 +19,18 @@ const task_descriptions = {
     "MedCaskTroll": "Be rewarded with a Med+ Casket that's worth LESS than 5k GP",
     "OborBryo": "Get a <a href='https://oldschool.runescape.wiki/w/Hill_giant_club' target='_blank'>Obor (Hill Giant) Club</a> or <a href='https://oldschool.runescape.wiki/w/Bryophyta%27s_essence' target='_blank'>Bryophyta's essence</a> drop",
 
-    "SkillingOutfit": "Earn any full <a href='https://oldschool.runescape.wiki/w/Skilling_equipment#List_of_outfits' target='_blank'>skilling outfit</a>", 
+    "SkillingOutfit": "Complete a full <a href='https://oldschool.runescape.wiki/w/Skilling_equipment#List_of_outfits' target='_blank'>skilling outfit</a>", 
     "MudSkipperDrops": "Get a <a href='https://oldschool.runescape.wiki/w/Mudskipper_hat' target='_blank'>Mudskipper hat</a> and <a href='https://oldschool.runescape.wiki/w/Flippers' target='_blank'>flippers</a> drops",
     "1stTimeFirecape": "First time Firecape",
     "PVMPet": "Earn any <a href='https://oldschool.runescape.wiki/w/Pet#Boss_pets' target='_blank'>PvM Pet</a>",
     "GWDrop": "Any unique drop from any of the  <a href='https://oldschool.runescape.wiki/w/Godwars' target='_blank'>God Wars Dungeon</a> bosses.\
-    <br><br>Unique tables:\
+    <br><br>\
+    Unique tables:\
     <a href='https://oldschool.runescape.wiki/w/Kree%27arra#Uniques' target='_blank'>Kree'arra</a>,\
     <a href='https://oldschool.runescape.wiki/w/General_Graardor#Uniques' target='_blank'>General Graardor</a>,\
-    <a href='https://oldschool.runescape.wiki/w/Commander_Zilyana' target='_blank'>Commander Zilyana</a>,\
-    <a href='https://oldschool.runescape.wiki/w/K%27ril_Tsutsaroth' target='_blank'>Ril Tsutsaroth</a>,\
-    <a href='https://oldschool.runescape.wiki/w/Nex' target='_blank'>Nex</a>\
-    ",
+    <a href='https://oldschool.runescape.wiki/w/Commander_Zilyana#Uniques' target='_blank'>Commander Zilyana</a>,\
+    <a href='https://oldschool.runescape.wiki/w/K%27ril_Tsutsaroth#Uniques' target='_blank'>Ril Tsutsaroth</a>,\
+    <a href='https://oldschool.runescape.wiki/w/Nex#Uniques' target='_blank'>Nex</a>",
 
     "BarrowDrop": "Loot any unique <a href='https://oldschool.runescape.wiki/w/Barrows' target='_blank'>Barrows</a> piece",
     "DSpearOrLeftShield": "Get a <a href='https://oldschool.runescape.wiki/w/Dragon_spear' target='_blank'>Dragon Spear</a> or the <a href='https://oldschool.runescape.wiki/w/Shield_left_half' target='_blank'>Left Shield Halves</a>",
