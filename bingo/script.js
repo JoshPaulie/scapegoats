c077d0df16d8d650f0b5fa8e9b4bf3c85b5f5959
JoshPaulie <joshpaullee@gmail.com>
2022-05-17 09:18:58 -0500

    Broken tds

--- bingo/script.js
+++ bingo/script.js
@@ -1,5 +1,37 @@
+"use strict";
+
+const task_descriptions = {
+    "10WildyBoss": "Huh?",
+    "500kLootKey": "Earn a PK Loot Key worth 500k GP or more!",
+    // "10MedClue": "",
+    // "1mItemFlip": "",
+    // "SkillingPet": "",
+    // "2CarvedBones": "",
+    // "10QP": "",
+    // "50SlayerBossKC": "",
+    // "GnomeScarfDrop": "",
+    // "WildyBossDrop": "",
+    // "50VulkZulrah": "",
+    // "CrawlingHand": "",
+    // "500kRunecraft": "",
+    // "MedCaskTroll": "",
+    // "OborBryo": "",
+    // "SkillingOutfit": "",
+    // "MudSkipperDrops": "",
+    // "1stTimeFirecape": "",
+    // "PVMPet": "",
+    // "GWDrop": "",
+    // "BarrowDrop": "",
+    // "LeftSpearOrLeftShield": "",
+    // "500kAgility": "",
+    // "TemporossDrop": "",
+    // "500kHuner": "",
+}; 
+
 const taskInfoBox = document.getElementById("task-info");
 
-document.getElementById("500kLootKey").addEventListener("click", () => {
-	taskInfoBox.innerHTML = "Earn a PK Loot Key worth 500k GP or more!";
-});
+for ([task, description] of Object.entries(task_descriptions)) {
+    document.getElementById(task).addEventListener("click", () => {
+        taskInfoBox.innerHTML = description;
+    });
+}
