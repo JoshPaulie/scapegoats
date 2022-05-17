4dd035781522bb06affa8336d1483f441cfbefcf
JoshPaulie <joshpaullee@gmail.com>
2022-05-17 10:48:01 -0500

    Headway

--- bingo/script.js
+++ bingo/script.js
@@ -8,28 +8,35 @@ const task_descriptions = {
     "SkillingPet": "Earn any <a href='https://oldschool.runescape.wiki/w/Pet#Skilling_pets target='_blank''>skilling pet</a>",
 
     "2CarvedBones": "Get a <a href='https://oldschool.runescape.wiki/w/Curved_bone' target='_blank'>curved bone</a> drop",
-    "10QP": "Earn 10 additional Quest Points",
+    "10QP": "Earn 10 additional Quest Points (as a team)",
     "50SlayerBossKC": "Get 50 <a href='https://oldschool.runescape.wiki/w/Boss#Slayer_bosses' target='_blank'>Slayer Boss</a> kills",
     "GnomeScarfDrop": "Get a <a href='https://oldschool.runescape.wiki/w/Gnome_scarf' target='_blank'>Gnome Scarf</a> drop",
     "WildyBossDrop": "Get any unique <a href='https://oldschool.runescape.wiki/w/Wilderness#Bosses' target='_blank'>Wilderness Boss</a> drop",
 
-    "50VulkZulrah": "Get 50 Vorkath OR  ",
-    "CrawlingHand": "",
+    "50VulkZulrah": "Get 50 <a href='https://oldschool.runescape.wiki/w/Vorkath' target='_blank'>Vorkath</a> and/or <a href='https://oldschool.runescape.wiki/w/Zulrah' target='_blank'>Zulrah</a> KC ",
+    "CrawlingHand": "Get a <a href='https://oldschool.runescape.wiki/w/Crawling_hand_(item)' target='_blank'>crawling hand</a> drop",
     "500kRunecraft": "Earn 500k Runecrafting Experience",
     "MedCaskTroll": "Be rewarded with a Med+ Casket that's worth LESS than 5k GP",
-    "OborBryo": "Get a <a href='https://oldschool.runescape.wiki/w/Hill_giant_club' target='_blank'>Obor (Hill Giant) Club</a> OR <a href='https://oldschool.runescape.wiki/w/Bryophyta%27s_essence' target='_blank'>Bryophyta's essence</a> drop",
+    "OborBryo": "Get a <a href='https://oldschool.runescape.wiki/w/Hill_giant_club' target='_blank'>Obor (Hill Giant) Club</a> or <a href='https://oldschool.runescape.wiki/w/Bryophyta%27s_essence' target='_blank'>Bryophyta's essence</a> drop",
 
     "SkillingOutfit": "Earn any full <a href='https://oldschool.runescape.wiki/w/Skilling_equipment#List_of_outfits' target='_blank'>skilling outfit</a>", 
-    "MudSkipperDrops": "Get a Mudskipper <a href='https://oldschool.runescape.wiki/w/Mudskipper_hat' target='_blank'>hat</a> AND <a href='https://oldschool.runescape.wiki/w/Flippers' target='_blank'>flippers</a> drops",
+    "MudSkipperDrops": "Get a <a href='https://oldschool.runescape.wiki/w/Mudskipper_hat' target='_blank'>Mudskipper hat</a> and <a href='https://oldschool.runescape.wiki/w/Flippers' target='_blank'>flippers</a> drops",
     "1stTimeFirecape": "First time Firecape",
     "PVMPet": "Earn any <a href='https://oldschool.runescape.wiki/w/Pet#Boss_pets' target='_blank'>PvM Pet</a>",
-    "GWDrop": "Any <a href='https://oldschool.runescape.wiki/w/Pet#Boss_pets' target='_blank'>God Wars Dungeon</a> drop",
+    "GWDrop": "Any unique drop from any of the  <a href='https://oldschool.runescape.wiki/w/Godwars' target='_blank'>God Wars Dungeon</a> bosses.\
+    <a href='https://oldschool.runescape.wiki/w/Kree%27arra#Uniques' target='_blank'>Fish Barrel</a>,\
+    ",
 
     "BarrowDrop": "Loot any unique <a href='https://oldschool.runescape.wiki/w/Barrows' target='_blank'>Barrows</a> piece",
-    "LeftSpearOrLeftShield": "",
-    "500kAgility": "",
-    "TemporossDrop": "",
-    "500kHuner": "",
+    "DSpearOrLeftShield": "Get a <a href='https://oldschool.runescape.wiki/w/Dragon_spear' target='_blank'>Dragon Spear</a> or the <a href='https://oldschool.runescape.wiki/w/Shield_left_half' target='_blank'>Left Shield Halves</a>",
+    "500kAgility": "Earn 500k Agility Experience",
+    "TemporossDrop": "Catch any of the following Tempoross uniques: \
+    <a href='https://oldschool.runescape.wiki/w/Fish_barrel' target='_blank'>Fish Barrel</a>,\
+    <a href='https://oldschool.runescape.wiki/w/Tackle_box' target='_blank'>Tackle Box</a>,\
+    <a href='https://oldschool.runescape.wiki/w/Big_harpoonfish' target='_blank'>Big harpoonfish</a>,\
+    <a href='https://oldschool.runescape.wiki/w/Tome_of_water#Empty' target='_blank'>Tome of water</a>,\
+     or <a href='https://oldschool.runescape.wiki/w/Tiny_tempor' target='_blank'>Tiny Tempor</a>",
+    "500kHunter": "Earn 500k Hunter Experience",
 }; 
 
 const taskInfoBox = document.getElementById("task-info");
